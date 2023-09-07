5477e75459dfdd15d58561904cd9904ab3a8a56c
rgb-griffith <ryan.bartlett@griffith.edu.au>
2023-09-07 20:34:59 +1000

    t2

--- js/sketch.js
+++ js/sketch.js
@@ -7,5 +7,27 @@ function setup(){
 function draw(){
     background(120,120,120);
     line(0,200,400,200);
-    triangle(20,20,20);
+    //if(mouseMoved){}
+    let topX=200;
+    let topY=200;
+    let leftX=topX+100;
+    let leftY=topY-100;
+    let rightX=topX-100;
+    let rightY=topY-100;
+    triangle(
+            leftX, leftY,
+            topX, topY, 
+            rightX, rightY);
+    
+    if(mouseMoved){
+        let topX=200;
+        let topY=200;
+        let leftX=topX-100;
+        let leftY=topY+100;
+        let rightX=topX+100;
+        let rightY=topY+100;
+        triangle(
+                leftX, leftY,
+                topX, topY, 
+                rightX, rightY);
 }
