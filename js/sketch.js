b2721b5a3844104ea8dffba5481f74592b3646e2
rgb-griffith <ryan.bartlett@griffith.edu.au>
2023-09-08 11:10:44 +1000

    unknown

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,6 @@
 "use strict";
+let rectX=0;
+let rectY=0;
 
 function setup(){
     createCanvas(400,400);
@@ -6,4 +8,8 @@ function setup(){
 
 function draw(){
     background(120,120,120);
+    rect(rectX,rectY,200,100);
+    console.log("rectX",rectX,rectY);
+    rectX+=5;
+    rectY+=5;
 }
