f7bb978f475f2f62768d0b7a98466993bf9f6ae8
rgb-griffith <ryan.bartlett@griffith.edu.au>
2023-09-08 11:14:06 +1000

    t1

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,13 @@
 "use strict";
 
+let imgs
+function preload() {
+  let img = loadImage('images/icon.png');
+}
+
 function setup(){
     createCanvas(400,400);
+    image(img,0,0);
 }
 
 function draw(){
