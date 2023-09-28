0c4c3e2520e11de5209f59b15e841532b4ef4345
rgb-griffith <ryan.bartlett@griffith.edu.au>
2023-09-28 15:27:49 +1000

    w

--- js/sketch.js
+++ js/sketch.js
@@ -5,9 +5,13 @@ let s;
 function setup(){
     new Canvas(800,800);
     s = new Sprite();
+    s.diameter = 50;
+    s.y = 50;
+
+    world.gravity.y = 1;
 }
 
 function draw(){
-    background(120,120,120);
+    background(255 ,255,255);
     
 }
