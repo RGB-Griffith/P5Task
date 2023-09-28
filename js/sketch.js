df8bd74ce6d06fc3809e77d148f064218de2b665
rgb-griffith <ryan.bartlett@griffith.edu.au>
2023-09-28 14:40:27 +1000

    w

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,18 @@
-"use strict";
+"use-strict";
 
-function setup(){
-    createCanvas(400,400);
+function setup() {
+    createCanvas(800, 600);
 }
 
-function draw(){
-    background(120,120,120);
+function draw() {
+    background(220);
+
+    fill(255, 0, 0);
+    stroke(0);
+    strokeWeight(2);
+    rect(width - 225, 25, 180, 80, 20);
+    fill(255,255,0);
+    ellipse(width - 75, 75, 40);
+    fill(0,0,255);
+    rect(150, 150, 500, 100, 20);
 }
