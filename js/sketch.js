170150aaaa7acb3f02eabdd94740ffd738a2718d
rgb-griffith <ryan.bartlett@griffith.edu.au>
2023-09-28 14:59:47 +1000

    why

--- js/sketch.js
+++ js/sketch.js
@@ -2,13 +2,15 @@
 
 
 let s;
-
+ 
 function setup(){
     new Canvas(800,800);
     s = new Sprite();
+    s.width = 100;
+    s.color = 'blue';
 }
-
+ 
 function draw(){
     background(120,120,120);
-    
+ 
 }
